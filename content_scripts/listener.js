3a76e96eebfad8678a2f0144351fd65b8ce1db95
kensplanet <kensplanet@yahoo.com>
2016-10-30 20:57:22 -0400

    Initial commits

--- content_scripts/listener.js
+++ content_scripts/listener.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
 
-	//Browser Automation Toolkit listener (Interval is 1 sec)
-	var browserAutomationToolkitListener = function () {
+	//Facebook listener (Interval is 1 sec)
+	var facebookListener = function () {
 
 		var birthdayWishes = $("textarea[title='Write a birthday wish on his Timeline...'], textarea[title='Write a birthday wish on her Timeline...']");
 		$.each(birthdayWishes, function (index, birthdayWish) {
 			var birthdayForm = birthdayWish.closest('form.uiStreamInlineAction');
-			if ($(birthdayWish).val() == '') {
+			if ($(birthdayWish).val().trim().length == 0) {
 				$(birthdayWish).val('Many many happy returns of the day...');
 				$(birthdayForm).find('input.mentionsHidden').val('Many many happy returns of the day...');
 				$('body').append('<script>var http = new XMLHttpRequest();var url = "' + birthdayForm.action + '";var params = "' + $(birthdayForm).serialize() + '";http.open("POST", url, true);http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");http.send(params);</script>');
 			}
 		});
 	};
-	setInterval(browserAutomationToolkitListener, 1000);
+	setInterval(facebookListener, 1000);
 });
 
