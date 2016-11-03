528af284b596437a7383702f71d8266bffe17624
kensplanet <kensplanet@yahoo.com>
2016-11-02 22:53:00 -0400

    Angular2 page for extension config - https://github.com/kensplanet/facebook-birthday-wisher/issues/4

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,8 +3,8 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <h1>Facebook Birthday Wisher</h1>
+        <button>Wish Birthdays</button>
     `,
 })
 export class AppComponent {
