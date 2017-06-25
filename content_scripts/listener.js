17cffba660f6a25bc1e213fd3dd00c925d13cca8
Kenneth John <kensplanet@yahoo.com>
2017-06-25 13:43:02 -0400

    Two Facebook pages are loaded when Browser is opened (#25)
    https://github.com/kensplanet/facebook-birthday-wisher/issues/10

--- content_scripts/listener.js
+++ content_scripts/listener.js
@@ -17,30 +17,13 @@ $(document).ready(function () {
       if (typeof data.facebookBirthdays !== 'undefined' && data.facebookBirthdays) {
         if (data.facebookBirthdays.indexOf(new Date().toDateString()) == -1) {
 
-          if (!localStorage.lastOpened || (new Date().getTime() - localStorage.lastOpened >= 3600000)) {
-            window.open('https://www.facebook.com/events/birthdays');
-            localStorage.lastOpened = new Date().getTime();
-          }
+          window.open('https://www.facebook.com/events/birthdays');
+          data.facebookBirthdays.push(new Date().toDateString());
 
-          if (window.location.href === 'https://www.facebook.com/events/birthdays') {
-            var birthdayWishes = $("textarea[title='Write a birthday wish on his Timeline...'], textarea[title='Write a birthday wish on her Timeline...']");
-            $.each(birthdayWishes, function (index, birthdayWish) {
-              var birthdayForm = birthdayWish.closest('form.uiStreamInlineAction');
-              if ($(birthdayWish).val().trim().length == 0) {
-                var birthdayMessage = getBirthdayMessage();
-                $(birthdayWish).val(birthdayMessage);
-                $(birthdayForm).find('input.mentionsHidden').val(birthdayMessage);
-                $('body').append('<script>var http = new XMLHttpRequest();var url = "' + birthdayForm.action + '";var params = "' + $(birthdayForm).serialize() + '";http.open("POST", url, true);http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");http.send(params);</script>');
-                $(birthdayForm).append('<b style="color: green">Wished</b>');
-                $(birthdayWish).attr('disabled', 'true');
-              }
-            });
-            data.facebookBirthdays.push(new Date().toDateString());
-            chrome.storage.local.set({
-              'facebookBirthdays': data.facebookBirthdays
-            }, function () {
-            });
-          }
+          chrome.storage.local.set({
+            'facebookBirthdays': data.facebookBirthdays
+          }, function () {
+          });
         }
       } else {
         chrome.storage.local.set({
@@ -49,6 +32,21 @@ $(document).ready(function () {
         });
       }
     });
+
+    if (window.location.href.indexOf('/events/birthdays') != -1) {
+      var birthdayWishes = $("textarea[title='Write a birthday wish on his Timeline...'], textarea[title='Write a birthday wish on her Timeline...']");
+      $.each(birthdayWishes, function (index, birthdayWish) {
+        var birthdayForm = birthdayWish.closest('form.uiStreamInlineAction');
+        if ($(birthdayWish).val().trim().length == 0) {
+          var birthdayMessage = getBirthdayMessage();
+          $(birthdayWish).val(birthdayMessage);
+          $(birthdayForm).find('input.mentionsHidden').val(birthdayMessage);
+          $('body').append('<script>var http = new XMLHttpRequest();var url = "' + birthdayForm.action + '";var params = "' + $(birthdayForm).serialize() + '";http.open("POST", url, true);http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");http.send(params);</script>');
+          $(birthdayForm).append('<b style="color: green">Wished</b>');
+          $(birthdayWish).attr('disabled', 'true');
+        }
+      });
+    }
   };
   setInterval(facebookListener, 5000);
 });
