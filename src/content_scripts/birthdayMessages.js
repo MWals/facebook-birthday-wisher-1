d5a41bdb06acb283d0b4b945640c8ca1b28d73d6
Kenneth John <kensplanet@yahoo.com>
2017-08-25 11:07:32 -0400

    Fix the Birthday Message (#43)
    https://github.com/kensplanet/facebook-birthday-wisher/issues/39

--- src/content_scripts/birthdayMessages.js
+++ src/content_scripts/birthdayMessages.js
@@ -3,7 +3,7 @@ var birthdayMessages = [
   'Well, you are another year older and you haven\'t changed a bit. That\'s great because you are perfect just the way you are. Happy Birthday.',
   'Have a wonderful birthday. I wish your every day to be filled with lots of love, laughter, happiness and the warmth of sunshine.',
   'May your coming year surprise you with the happiness of smiles, the feeling of love and so on. I hope you will find plenty of sweet memories to cherish forever. Happy birthday.',
-  'On your special day, I wish you good luck. I hope this wonderful day will fill up your heart with joy and blessings. Have a fantastic birthday, celebrate the happiness on every day of your life. Happy Birthday!!,' +
+  'On your special day, I wish you good luck. I hope this wonderful day will fill up your heart with joy and blessings. Have a fantastic birthday, celebrate the happiness on every day of your life. Happy Birthday!!',
   'Special day, special person and special celebration. May all your dreams and desires come true in this coming year. Happy birthday.',
   'Soon you’re going to start a new year of your life and I hope this coming year will bring every success you deserve. Happy birthday.',
   'I hope your birthday is going to be a terrific one. Enjoy your day and be happy always. Happy birthday.',
