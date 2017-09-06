c860c0971c9aaf78927a3a6837073bd9c57ef886
Kenneth John <kensplanet@yahoo.com>
2017-09-06 19:29:15 -0400

    Implement accordion for help (#62)
    https://github.com/kensplanet/facebook-birthday-wisher/issues/61

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { SettingsComponent } from './settings/settings.component';
+import {AccordionModule} from "ngx-accordion";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -23,7 +24,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
-    BrowserModule
+    BrowserModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
